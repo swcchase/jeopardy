7330c94a6a42b5c817cf41dd723ad3d4ce914690
Cody <ctchase10@gmail.com>
2022-04-14 20:17:00 -0400

    add

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 initCatRow();
 initBoard();
 
+document.querySelector("button").addEventListener("click", buildCategories);
+
+//CREATE CATEGORY ROW
+
 function initCatRow() {
   let catRow = document.getElementById("category-row");
 
@@ -11,6 +15,8 @@ function initCatRow() {
     catRow.appendChild(box);
   }
 }
+
+//CREATE CLUE BOARD
 
 function initBoard() {
   let board = document.getElementById("clue-board");
@@ -38,3 +44,123 @@ function initBoard() {
 function getClue() {
   console.log("have a nice day");
 }
+//CALL API
+
+function randInt() {
+  return Math.floor(Math.random() * 18418 + 1);
+}
+
+function buildCategories() {
+  const fetchReq1 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq2 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq3 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq4 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq5 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq6 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const allData = Promise.all([
+    fetchReq1,
+    fetchReq2,
+    fetchReq3,
+    fetchReq4,
+    fetchReq5,
+    fetchReq6,
+  ]);
+
+  allData.then((res) => {
+    console.log(res);
+  });
+}
+
+function getClue() {
+  console.log("have a nice day");
+}
+let catArray = [];
+
+function buildCategories() {
+  const fetchReq1 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq2 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq3 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq4 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq5 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq6 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const allData = Promise.all([
+    fetchReq1,
+    fetchReq2,
+    fetchReq3,
+    fetchReq4,
+    fetchReq5,
+    fetchReq6,
+  ]);
+
+  allData.then((res) => {
+    console.log(res);
+    catArray = res;
+    setCategories(catArray);
+  });
+}
+
+//LOAD CATEGORIES TO THE BOARD
+
+function setCategories(catArray) {
+  let element = document.getElementById("category-row");
+  let children = element.children;
+  for (let i = 0; i < children.length; i++) {
+    children[i].innerHTML = catArray[i].title;
+  }
+}
+
+//FIGURE OUT WHICH ITEM WAS CLICKED
+
+function getClue(event) {
+  let child = event.currentTarget;
+  child.classList.add("clicked-box");
+  let boxValue = child.innerHTML.slice(1);
+  let parent = child.parentNode;
+  let index = Array.prototype.findIndex.call(
+    parent.children,
+    (c) => c === child
+  );
+  let cluesList = catArray[index].clues;
+  let clue = cluesList.find((obj) => {
+    return obj.value == boxValue;
+  });
+  showQuestion(clue, child, boxValue);
+}
+
+//continue vod
